65d05185c197b265dc67f35922b99ea3264f6b5a
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-22 10:38:19 +0530

    Update and rename hello.c to name.c

---
+++ name.c
@@ -0,0 +1,9 @@
+// Write program to get name from user and print in the output
+
+#include<stdio.h>
+void main(){
+    char name[25];
+    printf("enter the name :");
+    scanf("\n%s",name );
+    printf("\n your name is %s", name);
+}
