1524861d8881c0206f1425f58730d37ff127415a
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-24 14:37:14 +0530

    Create Personal.js

---
+++ JavaScript/Personal.js
@@ -0,0 +1,29 @@
+// Write a program to get name , door no, street , place , district and pincode and print in below format
+
+const prompt = require("prompt-sync")();
+
+const Name = prompt("Name : " );
+
+const DoorNo = prompt("Doorno :");
+const a = parseInt(" DoorNo ");
+
+const Street = prompt("Street : " );
+
+const place = prompt("Place : " );
+
+const District = prompt("District : " );
+
+const Pincode = prompt("Pincode : ");
+const b = parseInt("Pincode");
+
+console.log( "Name :", Name );
+
+console.log("DoorNo :" , DoorNo );
+
+console.log("Street : ", Street);
+
+console.log("Place : ", place);
+
+console.log("District : ", District );
+
+console.log("Pincode : ", Pincode );
