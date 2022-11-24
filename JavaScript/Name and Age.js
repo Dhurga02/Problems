353d31720824a8abaeca50fc886d2d5dbed4b49b
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-24 14:12:32 +0530

    Create Name and Age.js

---
+++ JavaScript/Name and Age.js
@@ -0,0 +1,11 @@
+// Write program to get name and age from user and print in the output
+
+const prompt = require("prompt-sync")();
+
+const Name = prompt("enter your name : " );
+
+const Age = prompt("Enter your age : " );
+
+const x = parseInt(" Age ");
+
+console.log("You are " + Name , Age +" Years old" );
