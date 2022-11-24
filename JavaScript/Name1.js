a756e20595edaa5a7f6fe40831a2fd1de7ef4eef
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-24 13:48:56 +0530

    Create Name1.js

---
+++ JavaScript/Name1.js
@@ -0,0 +1,7 @@
+// Write program to get name from user and print it on console.
+
+const prompt = require("prompt-sync")();
+
+const Name = prompt("Enter your name : ");
+
+console.log("your name is ",Name);
