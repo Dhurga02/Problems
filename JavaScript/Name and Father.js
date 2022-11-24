8f8c6716f021e172696d9b9eff312f082cf4e274
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-24 11:48:05 +0530

    Create Name and Father.js

---
+++ JavaScript/Name and Father.js
@@ -0,0 +1,7 @@
+// Write program to get name and father name from user and print in the output
+
+console.log("Enter your name : dhurga ");
+
+console.log("enter your father name : saran ");
+
+console.log("hello dhurga saran ");
