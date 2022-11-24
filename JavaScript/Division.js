6f9303d3fa9dcb7d7281278d9e329d1204ca0fb5
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-24 15:37:16 +0530

    Create Division.js

---
+++ JavaScript/Division.js
@@ -0,0 +1,8 @@
+// Write a program to get two numbers and print its division 
+
+const prompt = require("prompt-sync")();
+
+const num1 = parseInt(prompt("Enter your num1 = " ));
+const num2 = parseInt(prompt("Enter your num2 = " ));
+
+console.log("Multiplication is = ", num1 / num2);
