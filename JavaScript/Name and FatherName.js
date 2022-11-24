9279ee037697b3496768f836160b0ddbafb44ba5
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-24 13:57:48 +0530

    Create Name and FatherName.js

---
+++ JavaScript/Name and FatherName.js
@@ -0,0 +1,9 @@
+// Write program to get name and father name from user and print in the output
+
+const prompt = require("prompt-sync")();
+
+const Name = prompt("enter your name : " );
+
+const FatherName = prompt("Enter your father name : " );
+
+console.log("Hello",Name + FatherName);
