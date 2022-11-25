ec07bb45fc35e5f345653ee3af4b2b1f47675145
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-25 10:12:08 +0530

    Create Total Chicken.js

---
+++ JavaScript/Total Chicken.js
@@ -0,0 +1,11 @@
+// Write a program to find the total chicken price. Input will be how many kilos of chicken, Output will be total chicken price.
+
+const prompt = require("prompt-sync")();
+
+const Chickens = parseInt(prompt("Enter the chicken's = "));
+
+const Kg = parseInt(prompt("Kg is = "));
+
+// const x = Chickens * Kg
+ 
+console.log("otal chicken price = ", Chickens * Kg );
