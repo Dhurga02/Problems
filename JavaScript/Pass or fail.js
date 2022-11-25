45e6da1489c1e8c6ca146c8c7d92893ddfd847f2
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-25 12:00:09 +0530

    Create Pass or fail.js

---
+++ JavaScript/Pass or fail.js
@@ -0,0 +1,12 @@
+// Find Student result based on total marks
+
+const prompt = require("prompt-sync")();
+
+const a = parseInt(prompt("Enter the total marks = "));
+
+if(a >= 35 ){
+    console.log(" pass ");
+
+}else{
+    console.log(" fail ");
+}
