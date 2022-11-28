6aa70045fffde5c19879d494364c54ffb33eb4dd
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-28 15:17:41 +0530

    Create N Odd number.js

---
+++ JavaScript/Foor Loop/N Odd number.js
@@ -0,0 +1,11 @@
+// Find the sum of all odd numbers between 1 to n.
+
+const prompt = require("prompt-sync")();
+
+const n = parseInt(prompt(" Enter the odd numbers = "));
+
+for( let i = 1 ; i <= n ; i = i + 2 ){
+
+    console.log( i );
+
+}
