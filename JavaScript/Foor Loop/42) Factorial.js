8e12133b7b7d47dd93afdb42faf21fceac64a63f
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-29 11:01:28 +0530

    Create 42) Factorial.js

---
+++ JavaScript/Foor Loop/42) Factorial.js
@@ -0,0 +1,14 @@
+// Factorial program
+
+// Get number from user and find its factorial 
+
+const prompt = require("prompt-sync")();
+
+const a = parseInt(prompt ("enter the factorial num = "));
+
+let b = 1
+
+for(let i = 1 ; i <=a ; i = i + 1){
+
+    console.log( b *= i );
+}
