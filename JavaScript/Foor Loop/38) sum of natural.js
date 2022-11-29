34d823dfa200f43765d460b2efa3122e1a9cc8bf
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-11-29 10:40:00 +0530

    Create 38) sum of natural.js

---
+++ JavaScript/Foor Loop/38) sum of natural.js
@@ -0,0 +1,12 @@
+// Find the sum of all natural numbers between 1 to n.
+
+const prompt = require("prompt-sync")();
+
+const n = parseInt(prompt("n = "));
+
+let x = 0
+
+for( let i = 1 ; i <=n ; i = i + 1 ){
+
+    console.log( x += i); 
+}
