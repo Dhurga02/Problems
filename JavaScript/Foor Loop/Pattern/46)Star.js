14652fb24141dfa03e2a5f7fdaa0e15d7b1d933e
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-01 16:13:46 +0530

    Create 46)Star.js

---
+++ JavaScript/Foor Loop/Pattern/46)Star.js
@@ -0,0 +1,25 @@
+// Pattern 2
+
+// N = 5
+
+// * 	*	*	*	*
+// *	*	*	*	*
+// *	*	*	*	*
+// *	*	*	*	*
+// *	*	*	*	*
+
+const prompt = require("prompt-sync")();
+
+const n = parseInt(prompt("Enter the number = "));
+
+let output = ""
+
+for(let i = 1 ; i<=n ; i = i + 1){
+
+    for(let j = 1 ; j<=n ; j = j + 1){
+
+        output = output + " * ";
+    }
+output = output + "\n";
+}
+console.log(output);
