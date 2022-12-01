ab93a315eff8b42263f3ef0bfeb32bc3b7c038f3
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-01 15:15:17 +0530

    Create 45) Pattern.js

---
+++ JavaScript/Foor Loop/Pattern/45) Pattern.js
@@ -0,0 +1,27 @@
+
+//  Pattern 1
+
+//  1 1 1 1 1
+//  2 2 2 2 2
+//  3 3 3 3 3
+//  4 4 4 4 4
+//  5 5 5 5 5 
+
+const prompt = require("prompt-sync")();
+
+const n = parseInt(prompt("enter the number = "));
+
+
+
+let output = " "
+
+for (let i = 1 ; i <= n; i = i + 1){
+
+for (let j = 1 ; j <= n; j = j + 1){
+
+    output = output + i + " ";
+}
+output = output +"\n";
+}
+
+console.log(output);
