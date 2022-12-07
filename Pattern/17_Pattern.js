071c843e325b8db337be4d2f9d29d83b18f77b02
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-07 15:56:27 +0530

    Update 17_Pattern.js

--- Pattern/17_Pattern.js
+++ Pattern/17_Pattern.js
@@ -19,9 +19,9 @@ for(let i=1 ; i<=n ; i=i+1){
     for(let j=1 ; j<=n ; j=j+1){
 
         if(j>=i){
-            output = output+j
+            output = output+"*  "
         }else{
-            output = output+" "
+            output = output+"   "
         }
        
     }
