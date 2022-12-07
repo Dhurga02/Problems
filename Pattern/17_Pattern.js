306810da0912e3a6bd591fbf27745eb85f7800fe
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-07 15:51:57 +0530

    17) pattern program

---
+++ Pattern/17_Pattern.js
@@ -0,0 +1,30 @@
+// Pattern 17
+
+// N = 5
+
+// *	*	*	*	
+// 	*	*	*	*
+// 		*	*	*
+// 			*	*
+// 				*
+
+const prompt = require("prompt-sync")();
+
+const n = parseInt(prompt("Enter the number = "));
+
+output = ""
+
+for(let i=1 ; i<=n ; i=i+1){
+
+    for(let j=1 ; j<=n ; j=j+1){
+
+        if(j>=i){
+            output = output+j
+        }else{
+            output = output+" "
+        }
+       
+    }
+    output = output+"\n"
+}
+console.log(output);
