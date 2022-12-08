8c95dfe5a52d1af727147bc173e82303b8ca20c0
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-08 16:12:17 +0530

    16) Pattern program

---
+++ Pattern/16_Pattern.js
@@ -0,0 +1,41 @@
+// Pattern 16
+
+// N = 5, , N will be always odd number and N > 3
+
+
+// *	*	*	*	*
+
+// *				*
+
+// *		*		*
+
+// *				*
+
+// *	*	*	*	*
+
+
+
+const prompt = require("prompt-sync")();
+
+const n = parseInt(prompt("Enter the number = "));
+
+let output = ""
+
+for(let i = 1 ; i<=n ; i = i + 1){
+
+    for(let j = 1 ; j<=n ; j = j + 1){
+
+if(i==1 || i==n || j==1 || j==n || i==4&&j==4){
+    output= output+"* "
+
+
+}
+else{
+    output=output+"  "
+}
+ 
+    }
+output = output + "\n";
+}
+console.log(output);
+
