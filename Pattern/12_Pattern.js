be649996219765a514af175d650bdf9472d74c91
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-13 11:33:53 +0530

    Create 12_Pattern.js

---
+++ Pattern/12_Pattern.js
@@ -0,0 +1,35 @@
+
+// Pattern 12
+
+// name = VICKY
+
+// Output:
+
+// V
+
+// V     I 
+
+// V     I      C
+
+// V    I      C    K
+
+// V     I      C    K      Y
+
+const prompt = require("prompt-sync")();
+
+const input = "VICKY";
+
+string = "";
+
+for(let i=0; i<input.length; i++){
+
+    for(let j=0;j<=i;j++){
+
+        string=string +` ${input[j]}`;
+
+    }
+    string=string+"\n";
+
+}
+
+console.log(string);
