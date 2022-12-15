70cdf8f5e359336300d931577aad3183a9c1c630
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-15 15:29:48 +0530

    Create 1_Array.js

---
+++ Pattern/1_Array.js
@@ -0,0 +1,9 @@
+// Create family member names in one array variable and print one by one in the output
+
+
+let family = ["dhurga","saran","love","magic"];
+
+for(let i=0 ; i<=family.length ; i=i+1){
+
+    console.log(family[i]);
+}
