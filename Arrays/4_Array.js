68f343bcb47e4460289bf7a08c977de2f29a5a07
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-16 11:29:49 +0530

    Create 4_Array.js

---
+++ Arrays/4_Array.js
@@ -0,0 +1,17 @@
+
+// Create one array and find the middle element in the array. Assume array length is always odd number
+
+// Arr = [ 1 , 2 , 3 , 4 ,5 , 6 , 7 ] 
+
+// const prompt = require("prompt-sync")();
+
+// const num =parseInt(prompt("Eneter the mid number = "));
+
+
+ let Arr = [1,2,3,4,5,6,7];
+
+ const a = Math.floor(Arr.length/2);
+
+ const b = Arr[a];
+
+ console.log("The mid value is ",b);
