a54689936d38d1378c1d3f4942ce3c65164b7dac
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-19 11:44:56 +0530

    Create 7_Array.js

---
+++ Arrays/7_Array.js
@@ -0,0 +1,27 @@
+
+// Get List of student names from user and store it in the array
+
+// Enter total students: 4
+// Enter Student 1: Vignesh
+// Enter Student 2: Durga
+// Enter Student 3: Kumar
+// Enter Student 4: Vijay
+
+const prompt = require("prompt-sync")();
+
+const n =parseInt(prompt("Enter total student :"));
+
+let studentNames = [];
+studentNames[0]="vicky";
+studentNames[1]="dhurga";
+studentNames[2]="vijay";
+studentNames[3]="kumar";
+
+for(let i=0 ; i<n ; i=i+1){
+    for(let j=0 ; j<n ; j=j+1){
+        studentNames.push(studentNames);
+        
+}
+console.log(studentNames[i]);
+}
+
