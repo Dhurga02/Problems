27bae94d4447c1b36a76c93bc84ed6812b37847f
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-20 12:53:21 +0530

    Create 8_Array.js

---
+++ Arrays/8_Array.js
@@ -0,0 +1,27 @@
+// Get a list of numbers from users. Find the smallest ( minimum ) and largest ( maximum ) number in the array
+
+const prompt = require("prompt-sync")();
+
+let numbers=[];
+
+const n = parseInt(prompt("Ente the number : "));
+
+let min=numbers;
+let max=numbers;
+
+for(let i=1 ; i<=n ; i=i+1){
+    let number = prompt("the number is : ");
+    numbers.push(number);
+}
+
+for(let i=0 ; i< numbers.length ; i=i+1){
+let num=numbers[i];
+
+    if(num>max){
+        max=num;
+    }else if(num<min){
+        min=num;
+    }
+}
+console.log(`the maximum num is :  ${max}`);
+console.log(`the minimum num is :  ${min}`);
