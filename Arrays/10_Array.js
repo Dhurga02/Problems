ec0fbf99a2de2d015ef3e4dcbb8778c0b69a5bb5
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-21 17:06:03 +0530

    Create 10_Array.js

---
+++ Arrays/10_Array.js
@@ -0,0 +1,29 @@
+// Two sum problem 
+
+// Input: 
+
+// Arr = [ 1, 3, 4 , 8, 6 ]
+
+// Target = 9
+
+// Output: 
+
+// 1 + 8 = 9 function twoSum(arr, target)         
+
+// 3 + 6 = 9
+
+
+const prompt = require("prompt-sync")();
+
+const arr = [1,3,4,8,6];
+const target = 9;
+
+for(let i=0 ; i<arr.length ; i=i+1){
+    const num1 = arr[i];
+    for(let j=i ; j<arr.length ; j=j+1){
+        const num2 = arr[j];
+ if(num1 + num2 == target){
+             console.log(`${num1} + ${num2} = ${target}`);
+ }
+}
+}
