254d61924b3bcecb7364d60cc0fe6854c62084be
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2022-12-23 10:39:06 +0530

    Create 15_Array.js

---
+++ Arrays/15_Array.js
@@ -0,0 +1,14 @@
+// Remove non-vowels from the string
+
+// Input: Codewithvicky
+// Output: oeii
+
+const string="vignesh";
+let output=""
+
+for(let i=1 ; i<string.length ; i=i+1){
+    if("aeiouAeiou".includes(string[i])){
+        output=output+string[i];
+    }
+}
+console.log(output);
