9a90d14d742d5202ba5b4a7404f2a1b18e47ade7
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2023-01-03 13:00:03 +0530

    Create 1_Arr_Function.js

---
+++ Function/1_Arr_Function.js
@@ -0,0 +1,9 @@
+// Create family member names in one array variable and print one by one in the output
+
+const family = ["mom","dad","brother","sister"];
+function Names(name){
+    for(let i=0 ; i<name.length ; i=i+1){
+console.log(name[i]);
+    }
+}
+Names(family);
