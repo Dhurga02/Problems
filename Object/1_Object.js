5375a79b8ca8b12652422f155b05e70e7b3258f3
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2023-01-05 10:41:37 +0530

    Create 1_Object.js

---
+++ Object/1_Object.js
@@ -0,0 +1,15 @@
+// Create Student Object with following properties and print its values
+//    ID, Name, Email, Phone, Address - ( Street,  District, Pincode ) 
+
+const student={
+    ID:02,
+    Name:"dhurga",
+    Email:"dhuhrga@02gmail.com",
+    phone:"7806951517",
+    Address:{
+        Street:"Kovil street",
+        District:"Theni",
+        Pincode:"625512",
+    }
+}
+console.log(student);
