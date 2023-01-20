2623fd8639d34fdd101daf068a7ed1a0a96b8d06
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2023-01-20 15:34:41 +0530

    Update 3.3_weekly.js

--- weekly problem/3.3_weekly.js
+++ weekly problem/3.3_weekly.js
@@ -1,11 +1,12 @@
 // Remove 'K' elements from given array
+const prompt=require("prompt-sync")();
 function removeElement(a,k){
-        let Array=[];
-        for(let i=k ; i<a.length ; i=i+1){
-    Array.push(a[i]);
+    let Array=[];
+    for(let i=k ; i<a.length ; i=i+1){
+        Array.push(a[i]);
+    }
+    return Array;
 }
-return Array;
-}
-a=[1,2,3,4,5];
-k=1;
+let a=[1,2,3,4,5]
+let k=parseInt(prompt("Enter the element : "));
 console.log(removeElement(a,k));
