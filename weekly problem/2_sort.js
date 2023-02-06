2c1871405077999d66de61b7ac837b54596c3aa9
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2023-02-06 11:26:36 +0530

    Create 2_sort.js

---
+++ weekly problem/2_sort.js
@@ -0,0 +1,15 @@
+// write a program to sort an array
+
+function sortArray(arr){
+    let sort=arr.length;
+    for(let i=0 ; i<sort ; i=i+1){
+        for(let j=0 ; j<sort-i-1 ; j=j+1){
+            if(arr[j]>arr[j+1]){
+               [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+let number=[5,3,1,6,9,2,4,8];
+console.log(sortArray(number));
