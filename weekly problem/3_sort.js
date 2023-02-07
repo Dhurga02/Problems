ed4e88819308bc84b66cb86b5956b0c7bcd4a5fd
Dhurga02 <111416211+Dhurga02@users.noreply.github.com>
2023-02-07 16:33:35 +0530

    Create 3_sort.js

---
+++ weekly problem/3_sort.js
@@ -0,0 +1,17 @@
+function selectionSort(arr){
+    let n=arr.length;
+    for(let i=0 ; i<n ; i=i+1){
+        let min=i;
+        for(let j=i+1 ; j<n ; j=j+1){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        let temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+    return arr;
+ }
+ let number=[3,5,1,4,8,7,2];
+ console.log(selectionSort(number));
